a42974b6af97b27338093475ff70bb72864a14d9
ElvisStwar <elvis.stwar@hotmail.com>
2023-01-04 13:28:07 -0500

    Creacion del formulario e implementacion de validaciones

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
 
+
+export class HomeComponent implements OnInit{
+
+  public formLogin!: FormGroup;
+
+  constructor(private formBuilder:FormBuilder){
+
+  }
+
+  ngOnInit(): void{
+    this.formLogin = this.formBuilder.group({
+      email:['', [Validators.required]]
+    })
+  }
+
+  send():any{
+    console.log(this.formLogin.value)
+  }
 }
